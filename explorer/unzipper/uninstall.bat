fa9197f88aa2b60485bc45912152db37280129a1
Your Name <local@localhost>
2026-08-02 13:00:01 -0700

    Remove the explorer unzipper tool
    Deletes the C# "Unzip Here" shell extension along with the 29 bin/obj
    build artifacts that were being tracked alongside it.

--- explorer/unzipper/uninstall.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-reg delete "HKCU\Software\Classes\SystemFileAssociations\.zip\shell\UnzipHere" /f >nul 2>&1
-if exist "%LOCALAPPDATA%\UnzipHere" rmdir /s /q "%LOCALAPPDATA%\UnzipHere"
-
-echo Uninstalled.
-pause
